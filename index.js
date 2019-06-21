2bc996833cd3e95851e96e2e5287bde0000f8e93
ioanatarnauceanu <ioanatarnauceanu@icloud.com>
2019-06-21 19:27:31 +0300

    js

---
+++ index.js
@@ -0,0 +1,30 @@
+var age= 15;
+
+/*if (age>15 && age<25) {
+alert("10% discount")
+}
+else if {
+alert("25% discount")
+} */
+
+switch (age) {
+    case 15 
+    console.log(25% reducere);
+    break;
+    case 16
+    console.log(20% reducere);
+    break;
+    case 17
+    console.log(15% reducere);
+    break;
+    case 18
+    console.log(10% reducere);
+    break;
+    default
+    console.log(nu se aplica reducere);
+    break;
+}
+
+
+
+    
